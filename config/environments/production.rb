3b0fedb9d906755b87cb68d48f8bd8154a06c105
Alberto <alberto.grau.margarida@gmail.com>
2016-04-19 17:30:47 -0400

    heroku email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'nameless-fjord-74363.herokuapp.com' }
 end
