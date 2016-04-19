48432dc0ab87e79211b60fefb19bb2d5c0bd503e
Alberto <alberto.grau.margarida@gmail.com>
2016-04-19 17:26:24 -0400

    Scaffolded wikis

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  resources :wikis
+  #
+  # get 'wikis' => 'wikis#index'
+  # post 'wikis' => 'wikis#create'
+
   devise_for :users
+
+  root 'wikis#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
