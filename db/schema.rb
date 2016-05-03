0676d267cd4f7be30641e93e67d6ee56313b4eaf
Alberto <alberto.grau.margarida@gmail.com>
2016-05-03 08:27:03 -0400

    WIP updated wikipolicy

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160421211507) do
+ActiveRecord::Schema.define(version: 20160502193507) do
 
   create_table "amounts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "collaborators", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
